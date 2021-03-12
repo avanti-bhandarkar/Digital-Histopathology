bc9be83100746005008101be71358d4e8d039117
Avanti Bhandarkar <48968274+avanti-bhandarkar@users.noreply.github.com>
2021-03-12 22:02:21 +0530

    Update select non nucleus region.py

--- src/select non nucleus region.py
+++ src/select non nucleus region.py
@@ -5,11 +5,11 @@ fromCenter = False #Set default centering parameter of the cv2.selectROI to fals
 
 # Select region of interest from the loaded image
 print('Select a region with no nuclei in it and press enter/return to save it')
-img = cv2.imread("/Users/avantibhandarkar/Desktop/digital histopathology/ip images/pathology_slide_5.PNG") #Load image
+img = cv2.imread("ip images/pathology_slide_5.PNG") #Load image
 roi_nonu = cv2.selectROI(img, fromCenter, showCrosshair) #Run the default CV2 ROI function
 # Crop image to match seleted ROI
 nonucleus_region = img[int(roi_nonu[1]):int(roi_nonu[1]+roi_nonu[3]), int(roi_nonu[0]):int(roi_nonu[0]+roi_nonu[2])] #Select region between the top left and bottom right corner of the rectangle and crop it
 # Display cropped image
 cv2.imshow('Region with no nuclei',nonucleus_region)
-cv2.imwrite('/Users/avantibhandarkar/Desktop/digital histopathology/op images/nonucleus_region.png',nonucleus_region) #Save the cropped image
+cv2.imwrite('op images/nonucleus_region.png',nonucleus_region) #Save the cropped image
 cv2.waitKey(0) # Waits for user indefinitely to click enter key and exit external GUI windows
